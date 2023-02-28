6c8f42838eea3fb5b26102d99fae81474ce469ff
Jonathan Yanez <jonathanyanez02@gmail.com>
2023-02-27 21:42:10 -0500

    added files

---
+++ javascript.js
@@ -0,0 +1,17 @@
+console.log(22 + 59);
+
+
+let divide = "77";
+
+console.log((4+6+9) /divide);
+
+let a = 10;
+console.log(a);
+
+const string = "The revolution will not be televised.";
+console.log(string);
+
+const name = "Chris";
+const greeting = `Hello, ${name}`;
+console.log(greeting); // "Hello, Chris"
+
